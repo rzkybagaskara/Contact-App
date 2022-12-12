c2ed29c6e2b1cf0a2ad98767787eb2248584a54d
Rizky Bagaskara <rizky.6abu@hotmail.com>
2022-12-12 11:52:52 +0700

    menambahkan flash message ketika berhasil

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const session = require('express-session');
 const flash = require('connect-flash');
-const { body, check, validationResult } = require('express-validator');
 const cookieParser = require('cookie-parser');
 const connection = require('./model/connectDB');
 const bodyParser = require('body-parser');
-const { checkDuplicate } = require('./utils/check');
+const checkDuplicate = require('./utils/check');
 const app = express();
 const port = 8000;
 
@@ -14,7 +13,6 @@ app.set('view engine', 'ejs');
 // built in middleware
 app.use(express.static('public'));
 // parsing middleware
-// app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // konfigurasi flash message
@@ -37,14 +35,14 @@ app.use((req, res, next) => {
 
 // root page
 app.get('/', (req, res) => {
-  const sqlShow = 'SELECT * FROM table_kontak';
+  const sqlShow = 'SELECT * FROM table_kontak ORDER BY nama_lengkap ASC';
   connection.query(sqlShow, (err, result) => {
     const contacts = JSON.parse(JSON.stringify(result)); //parsing data
     res.render('contact', {
       title: 'Halaman Contact',
       layout: 'views/contact',
       contacts,
-      // msg: req.flash('msg'),
+      msg: req.flash('msg'),
     });
   });
 });
@@ -58,22 +56,30 @@ app.get('/addContact', (req, res) => {
 });
 
 // proses insert contact
+// gimana caranya biar si parser ke defined buat error
 app.post('/addContact', (req, res) => {
   const sqlAdd = `INSERT INTO table_kontak (npm, nama_lengkap, kelas, email) VALUES ( '${req.body.npm}', '${req.body.nama_lengkap}', '${req.body.kelas}', '${req.body.email}')`;
   connection.query(sqlAdd, (err, result) => {
     if (err) throw err;
-    // const duplicate = checkDuplicate();
-    // if (duplicate) throw new Error('NPM sudah ada di dalam daftar contact!');
-    // res.flash('msg', 'Data telah berhasil ditambahkan');
+    // if (err) {
+    //   req.flash('msg', 'NPM sudah ada di dalam database!');
+    // }
+    // // cek duplikat
+    // const duplicate = checkDuplicate(req.body.npm);
+    // if (duplicate) {
+    //   throw new Error('NPM sudah ada di dalam daftar contact!');
+    // }
+    req.flash('msg', 'Data telah berhasil ditambahkan');
     res.redirect('/');
   });
 });
 
-// intinya tinggal akses contact
+// halaman update contact
 app.get('/updateContact/:npm', (req, res) => {
   const npmSQL = `SELECT * FROM table_kontak WHERE npm = '${req.params.npm}'`;
   connection.query(npmSQL, (err, result) => {
     const contacts = JSON.parse(JSON.stringify(result));
+    // akses array contacts
     const contact = contacts[0];
     res.render('updateContact', {
       layout: 'views/updateContact',
@@ -88,7 +94,10 @@ app.post('/updateContact', (req, res) => {
   const sqlUpdate = `UPDATE table_kontak SET npm = '${req.body.npm}', nama_lengkap = '${req.body.nama_lengkap}', kelas = '${req.body.kelas}', email = '${req.body.email}' WHERE npm = '${req.body.oldNPM}'`;
   connection.query(sqlUpdate, (err, result) => {
     if (err) throw err;
-    // res.flash('msg', 'Data telah berhasil diupdate');
+    // if (err) {
+    //   req.flash('msg', 'NPM sudah ada di dalam database!');
+    // }
+    req.flash('msg', 'Data telah berhasil di-update');
     res.redirect('/');
   });
 });
@@ -98,9 +107,11 @@ app.get('/deleteContact/:npm', (req, res) => {
   const sqlDelete = `DELETE FROM table_kontak WHERE npm = '${req.params.npm}'`;
   connection.query(sqlDelete, (err, result) => {
     if (err) throw err;
+    req.flash('msg', 'Data telah berhasil dihapus');
     res.redirect('/');
   });
 });
+
 // about page
 app.get('/about', (req, res) => {
   res.render('about', {
